fcd298c13c4ba19e8f119bf721e873ec8c88c741
SXF-yb <1786493410@qq.com>
2024-04-18 19:09:22 +0800

    注册列表与注册缓存

--- 01-consumer-8080/src/main/java/com/bindada/controller/DepartController.java
+++ 01-consumer-8080/src/main/java/com/bindada/controller/DepartController.java
@@ -2,9 +2,12 @@ package com.bindada.controller;
 
 import com.bindada.pojo.Depart;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -13,11 +16,15 @@ public class DepartController {
     @Autowired
     private RestTemplate template;
 
+    @Autowired
+    private DiscoveryClient client;
+
     //直连方式
     //private  static final String SERVICE_PROCIER="http://localhost:8081/provider/depart";
 
     //微服务方式
     private  static final String SERVICE_PROCIER="http://depart-provider/provider/depart";
+
     @PostMapping("/save")
     public boolean saveHandle(@RequestBody Depart depart){
         String url = SERVICE_PROCIER+"/save";
@@ -43,5 +50,23 @@ public class DepartController {
     public List<Depart> listHandle(){
         String url = SERVICE_PROCIER+"/list";
         return template.getForObject(url,List.class);
+    }
+
+    @GetMapping("/discovery")
+    public List<String> discoveryHandle(){
+        List<String> services = client.getServices();
+        for (String serviceName : services) {
+            List<ServiceInstance> serviceInstances = client.getInstances(serviceName);
+            for (ServiceInstance instance : serviceInstances) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("serviceName",serviceName);
+                map.put("instanceId",instance.getInstanceId());
+                map.put("serviceId",instance.getServiceId());
+                map.put("serviceHost",instance.getHost());
+                map.put("serviceUrl",instance.getUri());
+                System.out.println(map);
+            }
+        }
+        return services;
     }
 }
--- 01-provider-8081/src/main/java/com/bindada/controller/DepartController.java
+++ 01-provider-8081/src/main/java/com/bindada/controller/DepartController.java
@@ -3,8 +3,11 @@ package com.bindada.controller;
 import com.bindada.pojo.Depart;
 import com.bindada.service.DepartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RequestMapping("/provider/depart")
@@ -12,6 +15,9 @@ import java.util.List;
 public class DepartController {
     @Autowired
     private DepartService service;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/save")
     public boolean saveHandle(@RequestBody  Depart depart){
@@ -36,5 +42,24 @@ public class DepartController {
     @GetMapping("/list")
     public List<Depart> listHandle(){
         return service.getAllDepart();
+    }
+
+
+    @GetMapping("/discovery")
+    public List<String> discoveryHandle(){
+        List<String> services = client.getServices();
+        for (String serviceName : services) {
+            List<ServiceInstance> serviceInstances = client.getInstances(serviceName);
+            for (ServiceInstance instance : serviceInstances) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("serviceName",serviceName);
+                map.put("instanceId",instance.getInstanceId());
+                map.put("serviceId",instance.getServiceId());
+                map.put("serviceHost",instance.getHost());
+                map.put("serviceUrl",instance.getUri());
+                System.out.println(map);
+            }
+        }
+        return services;
     }
 }
